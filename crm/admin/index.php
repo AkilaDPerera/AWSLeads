0e3907540aa2ce3e51980f22d3703a3b436feddb
Akila <akilamatrix@gmail.com>
2024-10-31 23:39:13 +0530

    update

--- crm/admin/index.php
+++ crm/admin/index.php
@@ -564,6 +564,7 @@
                                 searching: true,
                                 order: [[18, 'asc']],
                                 columnDefs: [
+                                    { target: 5, visible: false },
                                     { targets:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], className: "desktop" },
                                     { targets:[1, 6, 17], className: "mobile" },
                                     { target:17, visible: true, searchable: false}, { target:18, visible: false, searchable: false}
